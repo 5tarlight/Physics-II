23565c8418521cc5655b8ba1d0e1ad8b3e44af92
YEAHx4 <developerstarlight@gmail.com>
2023-04-09 21:57:59 +0900

    Force based movement

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 let canvas;
 let ctx;
+let pushBtn;
+let start = false;
 
 const BLACK = "rgb(0, 0, 0)";
 const RED = "rgb(200, 0, 0)";
@@ -21,10 +23,12 @@ class Item {
   y;
   w;
   h;
+  weight;
   color;
 
-  constructor(id, x, y, w, h = w, color = BLACK) {
+  constructor(id, weight, x, y, w, h = w, color = BLACK) {
     this.id = id;
+    this.weight = weight;
     this.x = x;
     this.y = y;
     this.w = w;
@@ -66,6 +70,7 @@ const R = {
 
     this.objs[index] = new Item(
       item.id,
+      item.weight,
       item.x + x,
       item.y + y,
       item.w,
@@ -79,8 +84,47 @@ const R = {
   },
 };
 
+class Force {
+  power;
+  theta; // θ (rad)
+  target;
+
+  constructor(target, power, theta = 0) {
+    this.power = power;
+    this.target = target;
+    this.theta = theta;
+  }
+}
+
+const forces = [];
+
+const addForce = () => {
+  start = true;
+  const f = new Force(0, 10, (-Math.PI * 1.5) / 4);
+  forces.push(f);
+};
+
+let velX = 0;
+let velY = 0;
+
+const calcAccel = () => {
+  while (forces.length > 0) {
+    const f = forces.pop();
+    const x = f.power * Math.cos(f.theta);
+    const y = f.power * Math.sin(f.theta);
+    console.log("move", f.target, "(", x, ",", y, ")");
+
+    velX += x / R.get(f.target).weight;
+    velY += y / R.get(f.target).weight;
+  }
+};
+
 const loop = () => {
-  R.move(0, 1, 0);
+  if (start) forces.push(new Force(0, 0.098, Math.PI / 2));
+  calcAccel();
+  R.move(0, velX, velY);
+
+  // console.log(R.objs[0]);
 
   clear();
   R.render();
@@ -90,8 +134,11 @@ const loop = () => {
 const setup = () => {
   canvas = document.querySelector("#canvas");
   ctx = canvas.getContext("2d");
+  pushBtn = document.querySelector("#push");
 
-  R.add(new Item(0, 50, 600, 100));
+  pushBtn.addEventListener("click", addForce);
+
+  R.add(new Item(0, 1, 50, 600, 100));
 
   console.dir(R.objs);
   console.log("Loading complete!");
